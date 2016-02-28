e27bf7c2d79a7aeba50097de369d1f58b3cc22f3
Antonija <ipekica@gmail.com>
2016-02-28 11:43:37 +0100

    First Commit

---
+++ Weather/Weather.swift
@@ -0,0 +1,21 @@
+//
+//  Weather.swift
+//  Weather
+//
+//  Created by Antonija Pek on 28/02/16.
+//  Copyright © 2016 Antonija Pek. All rights reserved.
+//
+
+import Foundation
+
+class Weather {
+    private var _temperature: Int!
+    
+    var temperature: Int {
+        return _temperature
+    }
+    
+    init() {
+        
+    }
+}
